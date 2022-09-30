0c0c6643f628e0364e309dd9872c3906eac08036
von-dracek <j.vondracek22@gmail.com>
2022-09-30 18:23:20 +0200

    Make output for cplex work

--- src/mean_cvar.py
+++ src/mean_cvar.py
@@ -9,36 +9,7 @@ import pandas as pd
 from gams import GamsWorkspace
 from tabulate import tabulate
 from src.configuration import CVAR_ALPHA, TICKERS
-
-def paralellized_create_gams_model_str(root):
-    n_jobs = 6  # TODO: parametrize this from command line
-    specified_row_name_length = 20
-    print(pd.to_datetime("now"))
-    def create_scenario_df(leaf):
-        scenario_series = []
-        for node in (leaf.ancestors + (leaf,)):
-            if node.depth > 0:
-                ser = pd.Series(node.returns, index=[f"stock{n + 1}" + " " * (
-                            specified_row_name_length - len(f"stock{n + 1}") - len(
-                        f".T{node.depth}")) + f".T{node.depth}" for n in range(len(node.returns))])
-                scenario_series.append(ser)
-        scenario = pd.concat(scenario_series)
-        scenario.name = leaf.name
-        return scenario
-
-    # with WorkerPool(n_jobs=n_jobs) as pool:
-    #     scenarios = pool.map(create_scenario_df, root.leaves)
-    scenarios = [create_scenario_df(leaf) for leaf in root.leaves]
-    scenario_df = pd.concat(scenarios, axis=1)
-    _str = f"""
-    Table
-    AssetYields(i, t, s)
-    {tabulate(scenario_df, headers="keys", tablefmt="plain", numalign="right", showindex=True, floatfmt=".4f")}
-    ;
-    """
-    print(pd.to_datetime("now"))
-    return _str
-
+import sys
 
 def create_cvar_gams_model_str(root):
     leaves = root.leaves
@@ -66,28 +37,10 @@ def create_cvar_gams_model_str(root):
     siblings = get_siblings(root)
     ssiblings = [{"node1":sibling[0], "time":sibling[1], "node2":sibling[2]} for sibling in siblings if sibling[0]!=sibling[2]]
     siblings = pd.DataFrame(ssiblings)
-    # df = pd.DataFrame(False, index=[leaf.name for leaf in root.leaves], columns=[leaf.name for leaf in root.leaves])
-    # dfs = []
-    # for t in range(root.height + 1):
-    #     df.loc[:,"time"] = t
-    #     _df = deepcopy(df)
-    #     dfs.append(_df)
-    # df = pd.concat(dfs)
-    # df = df.set_index("time", append=True)
-    #
-    # print(pd.to_datetime("now"))
-    # # df.loc[siblings.index.to_list(), siblings.iloc[:, 0].to_list()] = True
-    # for i, row in siblings.iterrows():
-    #     df.loc[i,row.iloc[0]]=True
-    # df = df * 1
-    # df.index = [
-    #     "   .T".join(map(str, col)) if not isinstance(col, str) else col for col in df.index
-    # ]
     sib = siblings["node1"].astype(str) + "." + siblings["node2"].astype(str) + ".T" + siblings["time"].astype(str)
     sib = sib.to_list()
     _str = f"""
 Option LP=CPLEX;
-option limrow = 10000 ;
 
 
 Set       s        / {", ".join([leaf.name for leaf in leaves])} / ;
@@ -111,14 +64,16 @@ Parameter
 InitialWealth /1/;
 
 Positive Variables x(i,s,t);
+x.l(i,s,t)=1/card(i);
 Variable loss;
 
-Equations          rootvariablessumuptoinitialwealth, nonanticipativityofx, objective;
+Equations          rootvariablessumuptoinitialwealth, nonanticipativityofx, xlessthanone, objective;
 
 rootvariablessumuptoinitialwealth(s).. InitialWealth=e=sum(i,x(i,s,"T0"));
 
-nonanticipativityofx(i,t,s,ss)$(siblings(s,ss,t))..  x(i,s,t) =e= x(i,ss,t);
+nonanticipativityofx(i,s,ss,t)$(siblings(s,ss,t))..  x(i,s,t) =e= x(i,ss,t);
 objective.. loss=e= sum(i,(sum(s,sum(t,x(i,s,t)))));
+xlessthanone(i,s,t).. x(i,s,t) =l= 1
 
 Model problem / ALL /;
 
@@ -130,5 +85,17 @@ solve problem using LP minimising loss;
     return _str
 
 def calculate_mean_cvar_over_leaves(root: Node) -> float:
-    # paralellized_create_gams_model_str(root)
-    create_cvar_gams_model_str(root)
+    gms = GamsWorkspace(system_directory="/opt/gams/gams40.2_linux_x64_64_sfx")
+    cvar_model_str = create_cvar_gams_model_str(root)
+    output_stream = io.StringIO()
+    job = gms.add_job_from_string(cvar_model_str)
+    job.run(output=output_stream)
+    x = {}
+    p = {}
+    output = output_stream.getvalue()
+
+    for rec in job.out_db["loss"]:
+        loss = rec.level
+    assert "Optimal solution found" in output or "** Feasible solution" in output
+    assert "*** Status: Normal completion" in output
+
