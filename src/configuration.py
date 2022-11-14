05942a0af683ae05d0528050a9c5bed31568effa
Jakub Vondráček <crasheru@gmail.com>
2022-11-14 13:14:52 +0100

    Delete unnecessary things from configuration.py

--- src/configuration.py
+++ src/configuration.py
@@ -6,7 +6,6 @@ LAST_VALID_DATE = datetime.datetime(2020, 12, 31, 23, 59)
 DOWNLOAD_DATA = False
 RETURNS_FILE = "returns"
 
-CVAR_ALPHA = 0.95
 MAX_NUMBER_LEAVES_IN_SCENARIO_TREE = 2000
 
 TICKERS = ['ACGL', 'AFL', 'AIG', 'AJG', 'ALL', 'AON', 'AXP', 'BAC', 'BEN', 'BK', 'BLK', 'BRO', 'C', 'CB', 'CINF', 'CMA', 'COF', 'FDS', 'FITB', 'GL', 'GS', 'HBAN', 'HIG', 'IVZ', 'JPM', 'KEY', 'L', 'LNC', 'MCO', 'MMC', 'MS', 'MTB', 'NTRS', 'PGR', 'PNC', 'RE', 'RF', 'RJF', 'SCHW', 'SIVB', 'SPGI', 'STT', 'TFC', 'TROW', 'TRV', 'USB', 'WFC', 'WRB', 'ZION']
@@ -15,7 +14,6 @@ TICKERS = ['ACGL', 'AFL', 'AIG', 'AJG', 'ALL', 'AON', 'AXP', 'BAC', 'BEN', 'BK',
 train_tickers = ['ALL', 'BK', 'ACGL', 'FDS', 'ZION', 'TROW', 'STT', 'RJF', 'SPGI', 'AON', 'LNC', 'USB', 'AXP', 'AIG', 'AJG', 'BLK', 'RE', 'SIVB', 'NTRS', 'CB', 'WRB', 'BRO', 'L', 'IVZ', 'BAC', 'GS', 'WFC', 'MTB', 'MMC', 'BEN']
 test_tickers = list(set(TICKERS).difference(set(train_tickers)))
 assert len(set(train_tickers).intersection(set(test_tickers)))==0
-TOTAL_N_STOCKS = len(TICKERS)
 
 class FILE(enum.Enum):
     RETURNS_FILE = "returns"
